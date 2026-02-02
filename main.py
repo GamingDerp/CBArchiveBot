f770fa968557418e2aa8ece46c5ae0a76093f325
GamingDerp <46617199+GamingDerp@users.noreply.github.com>
2026-02-01 21:27:47 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+import discord
+import asyncio
+from discord.utils import get
+from discord.ext import commands
+from datetime import datetime, timedelta
+import time
+
+intents = discord.Intents.default()
+intents.message_content = True  
+intents.guilds = True           
+intents.messages = True         
+intents.reactions = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+bot.remove_command('help')    
+bot.config = {
+    "server_id": 1466997651202637896,
+}
+
+async def load():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"cogs.{filename[:-3]}")
+            
+async def main():
+    await load()
+    await bot.start("TOKEN")
+    
+@bot.listen()
+async def on_ready():
+    await bot.change_presence(activity=discord.Game(name="Connecting CivBound.."))
+    await print(f"Logged in as {bot.user} \nID: {bot.user.id}")
+    await bot.tree.sync()
+
+asyncio.run(main())
